d4b6fb43191b20a32b73b9dc2e214925dabec3b1
yhl234 <200392342@student.georgianc.on.ca>
2019-09-05 17:46:27 -0400

    lab1 done

--- week1/lab1/js/app.js
+++ week1/lab1/js/app.js
@@ -5,14 +5,49 @@ const tweets = [];
 tweetButton.addEventListener("click", tweeting);
 function tweeting(e) {
 	e.preventDefault();
-	tweet = {
-		tweet: document.querySelector("#exampleFormControlTextarea1").value
-	};
+	tweet = hashtag(textArea.value);
 	tweets.unshift(tweet);
 	textArea.value = "";
-	console.log(tweets);
 	display(tweets);
 }
+
+// separate tweet to text, hashtag and no hashtag, and store into object
+//no hashtag is for href
+function hashtag(searchText) {
+	const regex = /\#\w+/g;
+	const hashtags = searchText.match(regex);
+	if (!hashtags) {
+		const tweet = {
+			tweet: searchText,
+			hashtag: [],
+			noHashtag: []
+		};
+		return tweet;
+	} else {
+		const index = searchText.indexOf("#");
+		const text = searchText.slice(0, index).trim();
+		const noHashtag = hashtags.map(hashtag => hashtag.replace("#", ""));
+		const tweet = {
+			tweet: text,
+			hashtag: hashtags,
+			noHashtag: noHashtag
+		};
+		return tweet;
+	}
+}
+
+function displayHashtag(array) {
+	let links = array
+		.map(item => {
+			return `
+		<a href=https://twitter.com/hashtag/${item}?src=hashtag_click target="_blank">#${item}</a>
+		`;
+		})
+		.join("");
+
+	console.log(links);
+}
+
 function display(tweets) {
 	let content = tweets
 		.map(tweet => {
@@ -25,7 +60,15 @@ function display(tweets) {
 					/>
 					<div class="pl-2"> 
 						<div>Louis L <span class="text-secondary ">@yhl123</span></div>
-						<div class="mt-1">${tweet.tweet}</div>
+						<div class="mt-1">${tweet.tweet} 
+						${tweet.noHashtag
+							.map(item => {
+								return `
+						<a href=https://twitter.com/hashtag/${item}?src=hashtag_click>#${item}</a>
+						`;
+							})
+							.join("")}
+						</div>
 						<div>
 							<span class="mdi mdi-comment-outline pt-3 pr-3 pb-3"></span>
 							<span class="mdi mdi-twitter-retweet p-3"></span>
